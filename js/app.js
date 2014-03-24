4e4942ce2233c6c8ef234d26677346c244d43a89
Oded Sagir <odedsagir@gmail.com>
2014-03-24 20:17:49 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+require(["js/config"], function (baseConfig) {
+    "use strict";
+
+    // Set the require base configurations
+    requirejs.config(baseConfig);
+
+    require(["marionette","bootstrap"], function(){
+        debugger;
+    });
+});
---
+++ js/config.js
@@ -0,0 +1,58 @@
+define({
+    paths:{
+        // Libraries
+        // ---------
+        "vendor":"bower_components",
+        "underscore":"bower_components/underscore/underscore",
+        "jquery":"bower_components/jquery/dist/jquery",
+        "backbone":"bower_components/backbone/backbone",
+        "backbone.wreqr":"bower_components/backbone.wreqr/lib/backbone.wreqr",
+        "backbone.babysitter":"bower_components/backbone.babysitter/lib/backbone.babysitter",
+        "marionette":"bower_components/marionette/lib/backbone.marionette",
+        "bootstrap":"bower_components/bootstrap/dist/js/bootstrap",
+
+        // Application Folders
+        // -------------------
+        /*"modules":"src/modules",
+        "models":"src/models",
+        "collections":"src/collections",
+        "controllers":"src/controllers",
+        "routers":"src/routers",
+        "layouts":"src/views/layouts",
+        "views":"src/views",
+        "templates":"assets/templates",*/
+    },
+    shim:{
+        "underscore":{
+            // Exports the global window._ object
+            "exports":"_"
+        },
+        "jquery":{
+            "exports":"$"
+        },
+        "backbone":{
+            "exports":"Backbone",
+            "deps":[
+                "underscore"
+            ]
+        },
+        "backbone.wreqr":{
+            "deps": ["backbone"]
+        },
+        "backbone.babysitter":{
+            "deps": ["backbone"]
+        },
+        "marionette":{
+            "exports":"Marionette",
+            "deps":[
+                "backbone",
+                "backbone.wreqr",
+                "backbone.babysitter"
+            ]
+        },
+        "bootstrap":{
+            "deps":["jquery"]
+        }
+
+    }
+});
